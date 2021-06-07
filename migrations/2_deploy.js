b1ca445b3782dee5d4a7f39cd95e8628a5cbf413
Bernardo A. Rodrigues <barodrigues@protonmail.com>
2021-06-07 20:36:51 -0300

    cleanup bootstrap

--- migrations/2_deploy.js
+++
@@ -1,7 +0,0 @@
-// migrations/2_deploy.js
-// SPDX-License-Identifier: MIT
-const ERC721PresetMinterPauserAutoId = artifacts.require("ERC721PresetMinterPauserAutoId");
-
-module.exports = function(deployer) {
-  deployer.deploy(ERC721PresetMinterPauserAutoId, "letter","LETT", "https://my-json-server.typicode.com/NFTletter/letter/tokens/");
-};
