43617da498dae1297f3f75c97fc99269ff439371
Bernardo A. Rodrigues <barodrigues@protonmail.com>
2021-06-15 18:54:22 -0300

    refactor Letter.sol

--- contracts/Letter.sol
+++ contracts/Letter.sol
@@ -5,79 +5,209 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 
 // Access Control
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
 // Safe Math
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract Letter is ERC721Upgradeable, Ownable, AccessControl {
+contract Letter is ERC721Upgradeable, OwnableUpgradeable, AccessControlUpgradeable {
 
-    // Safely count tokenIds
+    // Letter vars
+    string private _title;
+    string private _author;
+    bool private _open;
+    string[] private _pages; // Each page is a Token
+
+    // Safely count Pages
     using Counters for Counters.Counter;
-    Counters.Counter private tokenIds;
+    Counters.Counter private _pageCounter;
 
-    // Page struct
-    struct Page {
-        string body;
-    }
-
-    // Each page is a token
-    mapping (uint256 => Page) private page;
-
-    // Letter struct
-    struct Letter {
-        string title;
-        Page[] pages;
-        string author;
-        bool openView;
-    }
-
-    // Contract's Letter
-    Letter letter;
-
-    function initialize()
+    // ---------------------------------------
+    // Initializer
+    function createLetter(string memory _titleMint, string memory _firstPageMint, string memory _authorMint)
     public
-    initializer { 
-        __ERC721_init("Letter", "LETT");
+    initializer {
 
-        // init empty Letter struct
-        letter = Letter("", "", "", "");
-    }
+        __ERC721_init("Letter", "LETT");__ERC721_init("Letter", "LETT");
+        
+        // Page Body is mandatory, max 65536 characters
+        require((getStringLength(_firstPageMint) != 0), "Cannot mint Letter with an empty first Page");
+        require((getStringLength(_firstPageMint) <= 65536), "Page max length: 65536");
 
-
-    // function mintLetter()
-
-    // Access Control
-    using Roles for Roles.Role;
-    Roles.Role private _admins;
-    Roles.Role private _viewers;
-
-    // function mintAppendPage()
-
-    /// @dev Return `true` if the account belongs to the Viewer role.
-    function isAdmin(address _account)
-    public
-    virtual
-    view
-    returns (bool) {
-        return has(_admins, _account);
-    }
-
-    /// @dev Return `true` if the account belongs to the Viewer role.
-    function isViewer(address _account)
-    public
-    virtual
-    view
-    returns (bool) {
-
-        // Letter has openView?
-        if (letter.openView) {
-            return true;
+        // Page Title is optional, max 64 characters
+        if (getStringLength(_titleMint) != 0) {
+            require((getStringLength(_titleMint) <= 64), "Title max length: 64");
+            _title = _titleMint;
+        }
+        
+        // Page Author is optional, max 64 characters
+        if (getStringLength(_authorMint) != 0) {
+            require((getStringLength(_authorMint) <= 64), "Author max length: 64");
+            _author = _authorMint;
         }
 
-        return hasRole(_viewers, _account);
+        // Mint first Page
+        _mint(msg.sender, _pageCounter.current());
+
+        // Owner is Admin
+        grantRole(ADMIN_ROLE, msg.sender);
+
+        // Owner is Viewer
+        grantRole(VIEWER_ROLE, msg.sender);
+    }
+
+    // ---------------------------------------
+    // Access Control
+    bytes32 private constant ADMIN_ROLE = keccak256("ADMIN");
+    bytes32 private constant VIEWER_ROLE = keccak256("VIEWER");
+
+    modifier onlyViewer() {
+        if (!_open){
+            require (hasRole(VIEWER_ROLE, msg.sender), "Restricted to Viewer Role");
+        }
+        _;
+    }
+
+    /// @dev Add an account to the Admin role of _letterId. Restricted to Admins.
+    function addAdmin(address _account)
+    public
+    virtual
+    onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(ADMIN_ROLE, _account);
+    }
+
+    /// @dev Remove oneself from the admin role.
+    function renounceAdmin()
+    public
+    virtual {
+        renounceRole(ADMIN_ROLE, msg.sender);
+    }
+
+    /// @dev Add an account to the Viewer role. Restricted to Admins.
+    function addViewer(address _account)
+    public
+    virtual
+    onlyRole(ADMIN_ROLE) {
+        grantRole(VIEWER_ROLE, _account);
+    }
+
+    /// @dev Remove an account from the Viewer role. Restricted to admins.
+    function removeViewer(address _account)
+    public
+    virtual
+    onlyRole(ADMIN_ROLE) {
+        revokeRole(VIEWER_ROLE, _account);
+    }
+
+    /// @dev open Viewer Role (every account can View)
+    function openView()
+    public
+    virtual
+    onlyRole(ADMIN_ROLE) {
+        _open = true;
+    }
+
+    /// @dev close Viewer Role (only Viewer accounts can View)
+    function closeView()
+    public
+    virtual
+    onlyRole(ADMIN_ROLE) {
+        _open = false;
+    }
+
+    // ---------------------------------------
+    // View functions
+    
+    /// @dev view Title
+    function viewTitle()
+    public
+    view
+    onlyViewer
+    returns (string memory) {
+        return _title;
+    }
+
+    // @dev view Author
+    function viewAuthor()
+    public
+    view
+    onlyViewer
+    returns (string memory) { 
+        return _author;
+    }
+
+    // @dev view Page
+    function viewPage(uint256 _pageN)
+    public
+    view
+    onlyViewer
+    returns (string memory) {
+        require ((_pageN <= _pageCounter.current()), "Can't view non-existent Page");
+        return _pages[_pageN];
+    }
+
+    // @dev view Page Count
+    function viewPageCount()
+    public
+    view
+    onlyViewer
+    returns (uint256) {
+        return _pageCounter.current();
+    }
+
+    // @dev is open?
+    function isOpen()
+    public
+    view
+    returns (bool) {
+        return _open;
+    }
+
+    // ---------------------------------------
+    // Utility functions
+
+    /// @dev count string length
+    function getStringLength(string memory _s) 
+    private
+    pure
+    returns (uint) {
+        bytes memory bs = bytes(_s);
+        return bs.length;
+    }
+
+    // ---------------------------------------
+    // Mint new Page function
+
+    /// @dev mint new Page, append to Letter
+    function mintAppendPage(string memory _pageMint)
+    public
+    onlyOwner
+    returns (uint256)
+    {
+        // Page Body is mandatory, max 65536 characters
+        require((getStringLength(_pageMint) != 0), "Cannot mint empty page");
+        require((getStringLength(_pageMint) <= 65536), "Page max length: 65536");
+
+        // increment page counter
+        _pageCounter.increment();
+        uint256 newPageN = _pageCounter.current();
+
+        // Mint Page Token
+        _mint(msg.sender, newPageN);
+
+        // Return minted Page Number
+        return newPageN;
+    }
+
+    /// @dev override
+    function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControlUpgradeable, ERC721Upgradeable) returns (bool) {
+        return
+            interfaceId == type(IAccessControlUpgradeable).interfaceId ||
+            interfaceId == type(IERC721Upgradeable).interfaceId ||
+            interfaceId == type(IERC721MetadataUpgradeable).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 
 }
