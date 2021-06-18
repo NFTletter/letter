d67d6c1b9abdb1532355f00988e118c14e07fb94
Bernardo A. Rodrigues <barodrigues@protonmail.com>
2021-06-17 21:35:08 -0300

    deploy LetterFactory on migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Letter = artifacts.require("Letter");
+const LetterFactory = artifacts.require("LetterFactory");
 
 module.exports = function(deployer) {
   deployer.deploy(Letter);
+  deployer.deploy(LetterFactory);
 };
