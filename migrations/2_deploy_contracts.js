82f118fef11c5564740b8d1c3679e400c01d2218
Bernardo A. Rodrigues <barodrigues@protonmail.com>
2021-06-18 17:08:38 -0300

    cleanup truffle

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const Letter = artifacts.require("Letter");
-const LetterFactory = artifacts.require("LetterFactory");
-
-module.exports = function(deployer) {
-  deployer.deploy(Letter);
-  deployer.deploy(LetterFactory);
-};
